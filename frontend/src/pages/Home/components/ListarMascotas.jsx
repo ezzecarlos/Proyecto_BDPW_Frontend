6372fde91bf1ba29671d3557666f68387da85e12
ezzecarlos <145931098+ezzecarlos@users.noreply.github.com>
2023-10-22 03:56:24 -0300

    listado de 5 perros y gatos
    código editado para mas mascotas y comentarios de que hace lo nuevo.

--- frontend/src/pages/Home/components/ListarMascotas.jsx
+++ frontend/src/pages/Home/components/ListarMascotas.jsx
@@ -7,15 +7,18 @@ function ListarMascotas() {
   useEffect(() => {
     const getData = async () => {
       try {
+        // Realiza nuevas solicitudes de datos y agrégalas al listado existente
         const responses = await Promise.all([
-          fetch("https://api.thecatapi.com/v1/images/search"),
-          fetch("https://api.thedogapi.com/v1/images/search"),
+          fetch("https://api.thecatapi.com/v1/images/search?limit=5"), // Solicita 5 imágenes de gatos adicionales
+          fetch("https://api.thedogapi.com/v1/images/search?limit=5"), // Solicita 5 imágenes de perros adicionales
         ]);
 
         const data = await Promise.all(responses.map((response) => response.json()));
-        // Combina los resultados de gatos y perros en una sola lista
-        const mascotas = [...data[0], ...data[1]];
-        setListadoMascotas(mascotas);
+        
+        // Combina los nuevos resultados con el listado existente
+        const nuevasMascotas = [...data[0], ...data[1], ...listadoMascotas];
+        setListadoMascotas(nuevasMascotas);
+
         setBanderaCarga(false);
       } catch (error) {
         console.error("Hubo un error al obtener los datos:", error);
@@ -23,7 +26,7 @@ function ListarMascotas() {
     };
 
     getData();
-  }, []);
+  }, [listadoMascotas]); // Agrega listadoMascotas como una dependencia para que se actualice cuando cambie
 
   return (
     <div>
@@ -45,5 +48,6 @@ function ListarMascotas() {
     </div>
   );
 }
+
 
 export default ListarMascotas;
