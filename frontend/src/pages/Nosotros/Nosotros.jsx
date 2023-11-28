dafafb2ce232fba6c2b176d73182cbd8bc06283e
ezzecarlos <ezecarlos0@gmail.com>
2023-11-27 20:23:29 -0400

    Update Nosotros.jsx

--- frontend/src/pages/Nosotros/Nosotros.jsx
+++ frontend/src/pages/Nosotros/Nosotros.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import Typography from "@mui/material/Typography";
 
 function Nosotros() {
   return (
     <div>
-      <p></p>
+      <Typography
+            variant="h1"
+            component="h1"
+            sx={{
+              color: "dark",
+              fontFamily: "Arial",
+              fontWeight: 600,
+              letterSpacing: ".3rem",
+              textAlign: "center",
+              fontStyle: "bold",
+              marginRight: "20px"
+            }}
+          >
+            ADOPCIÓN DE <br />
+            ANIMALES
+          </Typography>
+      <Typography variant="body1">Este es el primer párrafo de texto.</Typography>
+      <Typography variant="body1">Este es el segundo párrafo de texto.</Typography>
     </div>
   );
-}
+};
 
 export default Nosotros;
+
